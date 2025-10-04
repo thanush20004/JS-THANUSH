fd23e02b56e95567ebd55de5d4dae36f6a7c5af7
Thanushkl <thanushkl20001@gmail.com>
2025-10-04 16:12:41 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1445432
+let  accountEmail = "thanush@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountState);
